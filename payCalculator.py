e7b3d5628d1a70754b6d3ef224a9d36ac85c577d
Ashton Bravo <Ashton.Bravo@vu.com>
2023-01-27 15:45:51 -0600

    test1

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,22 @@
-def calculatePay():
+#def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter hourly rate: "))
+
+#print (hrs)    
+if hrs <= 40 :
+    print("You didn't work more than 40 hours, no overtime pay. Your check will be $",(hrs*rate)) 
+else :
+    ot = hrs - 40
+    #print (ot)
+    ot_pay = (rate * 1.5 * ot) 
+    #print (ot_pay)
+    normal_pay= 40*rate
+    print(normal_pay)
+    calcuationOT = normal_pay + ot_pay
+    print (calcuationOT)
+    #print ('You get overtime pay')
+    #print ()
     
     # end assignment
 
@@ -8,5 +24,5 @@ def calculatePay():
 ## run > python payCalculator.py
 
 #Ignore this for now. 
-if __name__ == "__main__":
-    calculatePay()
+#if __name__ == "__main__":
+   # calculatePay()
